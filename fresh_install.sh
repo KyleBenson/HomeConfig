6addfafb70555a3ef19400d562fda4290d783af5
Kyle Benson <kyle.edward.benson@gmail.com>
2013-01-22 11:21:29 -0800

    Added fresh_install script.

---
+++ fresh_install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Install all the dependencies for ns-3, as well as a bunch of other random stuff
+sudo apt-get install gcc g++ python ipython python-dev mercurial git bzr konsole gdb valgrind gsl-bin libgsl0-dev libgsl0ldbl flex bison libfl-dev tcpdump sqlite sqlite3 libsqlite3-dev emacs libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev vtun lxc uncrustify doxygen graphviz imagemagick texlive texlive-extra-utils texlive-latex-extra kile texlive-publishers texstudio python-sphinx dia python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev libboost-signals-dev libboost-filesystem-dev
